1f1d4834a274b43d7d5bfa66fa3397b668614cd6
Carlos Dias <86132535+Carlos-Eduardo-Dias@users.noreply.github.com>
2022-03-09 21:12:38 +0000

    criação da rota autores e função de inserção

--- models/autor.js
+++ models/autor.js
@@ -1,21 +1,26 @@
 const db = require("../db")
 
-class Autor{
-  static async inserir(){
+class Autor {
+  static async inserir(data) {
     const connect = await db.connect();
-    return await connect.query(" select * from autores")
+    const sql = "INSERT INTO autores(nome, sobrenome, data_nascimento) VALUES ($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.data_nascimento]
+    return await connect.query(sql, values);
   }
 
-  static selecionar(){
-    
+  static async selecionar() {
+    const connect = await db.connect();
+    return await connect.query(" select * from autores ")
   }
 
-  static atualizar(){
-    
+  static async atualizar() {
+    const connect = await db.connect();
+    return await connect.query(" ")
   }
 
-  static deletar(){
-    
+  static async deletar() {
+    const connect = await db.connect();
+    return await connect.query(" ")
   }
 }
 
