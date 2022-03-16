f150ec3c7dbdee25faad1cfbef86fd83d4acedae
Carlos Dias <86132535+Carlos-Eduardo-Dias@users.noreply.github.com>
2022-03-16 21:10:16 +0000

    Adicionei o update e o delete.

--- models/autor.js
+++ models/autor.js
@@ -13,14 +13,18 @@ class Autor {
     return await connect.query(" select * from autores ")
   }
 
-  static async atualizar() {
+  static async atualizar(data) {
     const connect = await db.connect();
-    return await connect.query(" ")
+    const sql = "UPDATE autores SET nome=$1, sobrenome=$2, data_nascimento=$3 WHERE id=$4";
+    const values = [data.nome, data.sobrenome, data.data_nascimento, data.id]
+    return await connect.query(sql, values);
   }
 
-  static async deletar() {
+  static async deletar(data) {
     const connect = await db.connect();
-    return await connect.query(" ")
+    const sql = "DELETE FROM autores WHERE id=$1";
+    const values = [data.id]
+    return await connect.query(sql, values);
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,39 @@ const Autor = require("../models/autor");
 
 /* Rota para autores */
 router.get('/', async function(req, res, next) {
-  const autores = await Autor.selecionar ();
+  const autores = await Autor.selecionar();
+  res.json(autores.rows);
+});
+
+router.get('/insert', async function(req, res, next) {
+
+  const autor = {
+    nome: "Rafael",
+    sobrenome: "Alves",
+    data_nascimento: "08/14/2007"
+  }
+  const autores = await Autor.inserir(autor);
+  res.json(autores.rows);
+});
+
+router.get('/update', async function(req, res, next) {
+
+    const autor = {
+    nome: "Pedro",
+    sobrenome: "Alves",
+    data_nascimento: "02/11/2003",
+    id: "4"
+  }
+  const autores = await Autor.atualizar(autor);
+  res.json(autores.rows);
+});
+
+router.get('/delete', async function(req, res, next) {
+
+    const autor = {
+    id: "5"
+  }
+  const autores = await Autor.deletar(autor);
   res.json(autores.rows);
 });
 
